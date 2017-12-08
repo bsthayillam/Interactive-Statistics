b0eb86f627ae0e108bdacf6b5458214837475a2e
Brenda Thayillam <bsthayillam@gmail.com>
2017-12-08 16:07:19 -0500

    Structured the application to follow project guidelines and our plots

--- server.R
+++ server.R
@@ -1,39 +1,34 @@
 function(input, output) {
   
-  output$main_plot <- renderPlot({
-    histogram <- ggplot(faithful, aes(x = eruptions, y = ..density..)) + 
-      geom_histogram(bins = as.numeric(input$n_breaks), fill = "white", color = "black") + 
-      labs(x = "Duration (minutes)", title = "Geyser eruption duration") + theme_bw()
+  output$dendrogram <- renderPlot({
     
-    scatter <- ggplot(faithful, aes(x = waiting, y = eruptions)) + 
-      geom_point(size = input$pt_adjust) +
-      labs(
-        title = "Distribution of Old Faithful Eruptions and Waiting Time",
-        x = "Waiting Time (mins)",
-        y = "Eruptions Time (mins)"
-      )
+  })
+  
+  output$choro <- renderPlot({
     
-    density <- ggplot(faithful, aes(x = waiting, y = eruptions)) + 
-      stat_density2d(h = c(input$bw1, input$bw2), aes(fill = ..density..),
-                     geom = "tile", contour = F) +
-      scale_fill_gradient2(low = "#430A4B", mid = "#64AE68", midpoint = 0.015, 
-                           high = "#FFC4BE") +
-      labs(
-        title = "Distribution of Old Faithful Eruptions and Waiting Time",
-        x = "Waiting Time (mins)",
-        y = "Eruptions Time (mins)"
-      )
+  })
+  
+  output$ts_plot <- renderPlot({
     
-    if (input$individual_obs) {
-      histogram <- histogram + geom_rug()
-    }
+  })
+  
+  output$txt_analysis <- renderPlot({
     
-    if (input$density) {
-      histogram <- histogram + geom_density(adjust = input$bw_adjust, color = "blue")
-    }
+  })
+  
+  output$scatter <- renderPlot({
     
-    print(histogram)
-    # print(scatter)
-    # print(density)
-  }) 
+  })
+  
+  output$histogram <- renderPlot({
+    
+  })
+  
+  output$state_bar <- renderPlot({
+    
+  })
+  
+  output$month_bar <- renderPlot({
+    
+  })
 }
--- ui.R
+++ ui.R
@@ -1,41 +1,43 @@
 library(shinythemes)
-
-bootstrapPage(
+library(plotly)
+library(threejs)
+library(dygraphs)
   
-  theme = shinytheme("darkly"),
-  
-  selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximate):",
-              choices = c(10, 20, 35, 50),
-              selected = 20),
-  
-  checkboxInput(inputId = "individual_obs",
-                label = strong("Show individual observations"),
-                value = FALSE),
-  
-  checkboxInput(inputId = "density",
-                label = strong("Show density estimate"),
-                value = FALSE),
-  
-  sliderInput(inputId = "pt_adjust",
-              label = "Point size adjustment:",
-              min = 1, max = 5, value = 1, step = 0.5),
-  
-  sliderInput(inputId = "bw1",
-              label = "Bandwidth 1 Adjustment:",
-              min = 0, max = 3, value = 1.5, step = 0.1),
-  
-  sliderInput(inputId = "bw2",
-              label = "Bandwidth 2 Adjustment:",
-              min = 0, max = 3, value = 1.5, step = 0.1),
-  
-  plotOutput(outputId = "main_plot", height = "300px"),
-  
-  # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-                   sliderInput(inputId = "bw_adjust",
-                               label = "Bandwidth adjustment:",
-                               min = 0.2, max = 2, value = 1, step = 0.2)
+navbarPage(
+    title = "Wildfire Risks 2010-2015",
+    
+    theme = shinytheme("darkly"),
+    
+    tabPanel("Clustering",
+             plotlyOutput(outputId = "dendrogram", height = "450px")
+    ),
+    
+    tabPanel("Chloropleth",
+             plotlyOutput(outputId = "choropleth", height = "450px")
+    ),
+    
+    tabPanel("Time Series",
+             plotlyOutput(outputId = "ts_plot", height = "450px")
+    ),
+    
+    tabPanel("Text Analysis", 
+             plotlyOutput(outputId = "txt_analysis", height = "450px")
+    ),
+    
+    tabPanel("Scatterplot",
+             plotlyOutput(outputId = "scatter", height = "450px")
+    ),
+    
+    tabPanel("Proportional Histogram",
+             plotlyOutput(outputId = "histogram", height = "450px")
+    ),
+    
+    tabPanel("Fires by State",
+             plotlyOutput(outputId = "state_bar", height = "450px")
+    ),
+    
+    tabPanel("Fire Size Across Months",
+             plotlyOutput(outputId = "month_bar", height = "450px")
+    )
   )
   
-)
